c5c2da29779e9589805be8ac53f6fec7133196c5
comeevery-git <comeevery@gmail.com>
2021-07-15 00:07:45 +0900

    IOS App init

---
+++ middle_todo/Model/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  middle_todo
+//
+//  Created by suhee❤️ on 2021/07/14.
+//
+
+import Foundation
+class Memo {
+    var content: String
+    var insertDate: Date
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummnyMemoList = [
+        Memo(content: "Create by middle Team."),
+        Memo(content: "Hello, Middle peoples.")
+    ]
+}
